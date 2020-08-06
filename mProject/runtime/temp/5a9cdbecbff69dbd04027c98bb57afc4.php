7b4b5c601337fae11a08241e1b1c3b09f992f596
lzy <953681426@qq.com>
2020-08-06 11:31:22 +0800

    提交ignore文件

---
+++ mProject/runtime/temp/5a9cdbecbff69dbd04027c98bb57afc4.php
@@ -0,0 +1,158 @@
+<?php /*a:2:{s:77:"D:\php\code\mobileProject\mProject\application\home\view\message\message.html";i:1596677076;s:68:"D:\php\code\mobileProject\mProject\application\home\view\parent.html";i:1596676150;}*/ ?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>超市 </title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="renderer" content="webkit">
+        <meta http-equiv="Cache-Control" content="no-siteapp" />
+        <link rel="icon" type="image/png" href="/static/home/theme/default/images/favicon.png">
+		<link href="/static/home/css/amazeui.min.css" rel="stylesheet" type="text/css" />
+		<link href="/static/home/css/style.css" rel="stylesheet" type="text/css" />
+		<link href="/static/home/css/check.css" rel="stylesheet" type="text/css" />
+		<script src="/static/home/js/jquery-1.10.2.min.js"></script>
+		<script src="/static/home/js/popup.mini.js"></script>
+		<style>
+			body{
+				background: #ececec;
+			}
+		</style>
+	</head>
+
+<body>
+
+		<header data-am-widget="header" class="am-header am-header-default sq-head ">
+			
+			<h1 class="am-header-title">
+  	            <a href="" class="">动态</a>
+            </h1>
+            <div class="am-header-right am-header-nav">
+				<a href="release.html" class="">
+					<i class="release">+</i>
+				</a>
+			</div>
+	    </header>
+	    <div style="height: 49px;"></div>
+	    <ul class="dontai" >
+	    	<li>
+	    		<div class="dt-top">
+	    			<div class="dt-top-left">
+	    				<img src="/static/home/images/my.png" width="60" height="60" /><br />
+	    				<a href=""><span>+</span>关注</a>
+	    			</div>
+	    			<div class="dt-top-right">
+	    				<p class="name">贝贝酱</p>
+	    				<p class="content"><a href="messdetail.html">为了展现学子的亮丽风采，促进校园文化发展，丰富大学校园生活，借此机会策划... ...</a></p>
+	    				<p class="dz">来自南昌大学前湖校区  <i class="time"> 10分钟前</i></p>
+	    			</div>
+	    		</div>
+	    		<ul class="dt-bot">
+	    			<li><a href=""><i class="iconfont">&#xe60d;</i>&nbsp;3</a></li>
+	    			<li><a href=""><i class="iconfont">&#xe60c;</i>&nbsp;5</a></li>
+	    			<li><a href="javascript:;" class="zf"><i class="iconfont">&#xe60e;</i>&nbsp;0</a></li>
+	    		</ul>
+	    	</li>
+	    	<li>
+	    		<div class="dt-top">
+	    			<div class="dt-top-left">
+	    				<img src="/static/home/images/my.png" width="60" height="60" /><br />
+	    				<a href=""><span>√</span>已关注</a>
+	    			</div>
+	    			<div class="dt-top-right">
+	    				<p class="name">贝贝酱</p>
+	    				<p class="content"><a href="messdetail.html">为了展现学子的亮丽风采，促进校园文化发展，丰富大学校园生活，借此机会策划... ...</a></p>
+	    				<p class="dz">来自南昌大学前湖校区  <i class="time"> 10分钟前</i></p>
+	    			</div>
+	    		</div>
+	    		<ul class="dt-bot">
+	    			<li><a href=""><i class="iconfont">&#xe60d;</i>&nbsp;3</a></li>
+	    			<li><a href=""><i class="iconfont">&#xe60c;</i>&nbsp;5</a></li>
+	    			<li><a href="javascript:;" class="zf"><i class="iconfont">&#xe60e;</i>&nbsp;0</a></li>
+	    		</ul>
+	    	</li>
+	    	<li>
+	    		<div class="dt-top">
+	    			<div class="dt-top-left">
+	    				<img src="/static/home/images/my.png" width="60" height="60" /><br />
+	    				<a href=""><span>+</span>关注</a>
+	    			</div>
+	    			<div class="dt-top-right">
+	    				<p class="name">贝贝酱</p>
+	    				<p class="content"><a href="messdetail.html">为了展现学子的亮丽风采，促进校园文化发展，丰富大学校园生活，借此机会策划... ...</a></p>
+	    				<p class="dz">来自南昌大学前湖校区  <i class="time"> 10分钟前</i></p>
+	    			</div>
+	    		</div>
+	    		<ul class="dt-bot">
+	    			<li><a href=""><i class="iconfont">&#xe60d;</i>&nbsp;3</a></li>
+	    			<li><a href=""><i class="iconfont">&#xe60c;</i>&nbsp;5</a></li>
+	    			<li><a href="javascript:;" class="zf"><i class="iconfont">&#xe60e;</i>&nbsp;0</a></li>
+	    		</ul>
+	    	</li>
+	    </ul>
+	    <div class="share">
+	    	<div style="position: fixed; background: #ededed; bottom: 0; width: 100%; ">
+	    	<h2>转发</h2>
+	    	<ul class="share-style">
+	    		<li><a href=""><img src="/static/home/images/sqq.png" width="60"/><p>QQ</p></a></li>
+	    		<li><a href=""><img src="/static/home/images/sweixin.png" width="60" /><p>微信好友</p></a></li>
+	    		<li><a href=""><img src="/static/home/images/sweixinfriend.png" width="60"/><p>微信朋友圈</p></a></li>
+	    		<li><a href=""><img src="/static/home/images/skongjian.png" width="60" /><p>QQ空间</p></a></li>
+	    		<li><a href=""><img src="/static/home/images/ssina.png" width="60" /><p>新浪微博</p></a></li>
+	    	</ul>
+	    	<div class="popup-close">取消</div>
+	    	</div>
+	    </div>
+	
+<!--底部-->
+<div style="height: 55px;"></div>
+<div data-am-widget="navbar" class="am-navbar am-cf am-navbar-default sq-foot am-no-layout" id="">
+ <ul class="am-navbar-nav am-cf am-avg-sm-5">
+  <li>
+   <a href="index.html" class="curr">
+    <span class="am-icon-home"></span>
+    <span class="am-navbar-label">首页</span>
+   </a>
+  </li>
+  <li>
+   <a href="message.html" class="">
+    <span class="am-icon-comments"></span>
+    <span class="am-navbar-label">动态</span>
+   </a>
+  </li>
+  <li>
+   <a href="shopcart.html" class="">
+    <span class="am-icon-shopping-cart"></span>
+    <span class="am-navbar-label">购物车</span>
+   </a>
+  </li>
+  <li>
+   <a href="allorder.html" class="">
+    <span class="am-icon-file-text"></span>
+    <span class="am-navbar-label">订单</span>
+   </a>
+  </li>
+
+  <li>
+   <a href="member.html" class="">
+    <span class="am-icon-user"></span>
+    <span class="am-navbar-label">我的</span>
+   </a>
+  </li>
+ </ul>
+</div>
+ 
+ 
+
+ <script type='text/javascript'>
+	(function(){
+		 new PopUp_api({el:'.zf',html:'.share'});
+	})()
+</script>
+	
+</body>
+</html>
